ffadd80628a107057204cd0a6233c5328c1950e1
aliirah <alirahgoshy@gmail.com>
2022-03-17 03:18:55 +0330

    add url attributes in Url model

--- app/Models/Url.php
+++ app/Models/Url.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,4 +14,31 @@ class Url extends Model
         'destination',
         'slug'
     ];
+
+    /**
+     * @param $date
+     * @return string
+     */
+    public function getCreatedAtAttribute($date): string
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('m/d/Y, g:i A');
+    }
+
+    /**
+     * Get the user's first name.
+     *
+     * @return string
+     */
+    protected function getShortenUrlAttribute(): string
+    {
+        return \Illuminate\Support\Facades\URL::to('/') . "/{$this->slug}";
+    }
+
+    /**
+     * @return string
+     */
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
 }
